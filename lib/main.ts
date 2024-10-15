c0676f45f803c56a3705f1da6c384592663c674b
314oner <314oner@smtp.com>
2024-10-15 03:13:57 +0300

    removing unused CSS variables

--- lib/main.ts
+++ lib/main.ts
@@ -1,8 +1,8 @@
-import './global.css';
 export { DefaultAccordion } from './components/DefaultAccordion';
 export { DefaultAlert } from './components/DefaultAlert';
 export { DefaultAvatar } from './components/DefaultAvatar';
 export { DefaultBadge } from './components/DefaultBadge';
+export { DefaultButton } from './components/DefaultButton';
 export { DefaultCard } from './components/DefaultCard';
 export { DefaultCheckbox } from './components/DefaultCheckbox';
 export { DefaultDropdown } from './components/DefaultDropdown';
@@ -16,3 +16,4 @@ export { DefaultTooltip } from './components/DefaultTooltip';
 export { Input } from './components/Input';
 export { Label } from './components/Label';
 export { RefButton as DefaultLongButton, RefButton } from './components/RefButton';
+
